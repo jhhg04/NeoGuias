b617d31fcb27e316cad426daeb919183f0d385a0
jhhg0 <jhherrera@ccss.sa.cr>
2021-07-26 09:37:35 -0500

    Add Method 2

--- Array/duplicados.js
+++ Array/duplicados.js
@@ -1,15 +1,29 @@
 /* Method 1 */
-const numeros = [5, 1 , 2, 2, 7, 3, 4, 4, 7, 6]
-const tempArray = [...numeros].sort()
-let duplicados = []
 
-for(let i=0; i<tempArray.length; i++){
-  if(tempArray[i] === tempArray[i+1] ){
-    duplicados.push(tempArray[i])
+/*
+const numeros = [5, 1, 2, 2, 7, 3, 4, 4, 7, 6];
+const tempArray = [...numeros].sort();
+let duplicados = [];
+
+for (let i = 0; i < tempArray.length; i++) {
+  if (tempArray[i] === tempArray[i + 1]) {
+    duplicados.push(tempArray[i]);
   }
 }
 
-// console.log(duplicados)
+console.log(duplicados)
+*/
 
 /* Method 2 */
-const numeros = [5, 1 , 2, 2, 7, 3, 4, 4, 7, 6]
+const numeros = [5, 1, 2, 2, 7, 3, 4, 4, 7, 6];
+const numerosUnicos = [...new Set(numeros)];
+
+let duplicados = [...numeros];
+numerosUnicos.forEach((numero) => {
+  const indice = duplicados.indexOf(numero);
+  duplicados = duplicados
+    .slice(0, indice)
+    .concat(duplicados.slice(indice + 1, duplicados.length));
+});
+
+console.log(duplicados);
